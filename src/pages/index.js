1c46c9b4318f5404b80360d4436650b8ec6f104f
P1antain <lifeg1ance@yandex.ru>
2021-05-15 21:53:13 +0300

    refactoring Api > api

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,7 +5,7 @@ import Section from "../components/Section.js";
 import PopupWithImage from "../components/PopupWithImage.js";
 import PopupWithForm from "../components/PopupWithForm.js";
 import UserInfo from "../components/UserInfo.js";
-import Api from "../components/Api.js";
+import Api from "../components/api.js";
 import {popupOpenProfile, inputProfileName,
         inputProfileInfo, popupOpenImage,
         formElementProfile, formElementImage,
